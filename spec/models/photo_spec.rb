d2b92d796d71574dafe5e4407df299380a55963d
Sergio Díaz <sendorf@gmail.com>
2014-03-20 20:09:36 +0100

    Fixed an error in rspec

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -5,6 +5,6 @@ describe Photo do
   	photo = Photo.new
   	stock_ref = "ARNFH-U-5728"									# Sample stock reference
   	registration = "SO06DNV"									# Sample registration plate
-  	photo.generate_reference().should eq("AVRNNDF6H0-OUS2") 	# Generates the ofuscated reference for the example
-  end
+  	photo.generate_reference(stock_ref, registration).should eq("AVRNNDF6H0-OUS2") 	# Generates the ofuscated reference for
+  end																				# the example
 end
