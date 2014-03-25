1da4db268fca021a548fc8e227ab9f774f932c80
Sergio Díaz <sendorf@gmail.com>
2014-03-25 22:36:40 +0100

    Generates the candidate images for a car

--- app/models/search_engine.rb
+++ app/models/search_engine.rb
@@ -18,6 +18,18 @@ class SearchEngine
 		end
 	end
 
+	def generate_candidates(reference)
+		image_candidates = []
+		sizes = [350 , 800]
+		cameras = ['4', 'i', '6', 'f', '5', 'r']
+		sizes.each do |size|
+			cameras.each do |camera|
+				image_candidates << "http://imagecache.arnoldclark.com/imageserver/#{reference}/#{size}/#{camera}/"
+			end
+		end
+		image_candidates
+	end
+
 	def generate_reference(stock_ref = "", registration = "")
 		if(stock_ref.size !=0)
 			@stock_ref = stock_ref
@@ -30,16 +42,16 @@ class SearchEngine
 	  #stock_ref.delete("-")					# Removes '-' from stock_ref
 	  #registration.gsub(/\s+/, "")		# Removes spaces from registration
 	  #registration.delete("-")				# Removes '-' from registration
-		@registration.reverse!						# Reverses the registration for later proccessing
-		(0...@registration.size*2).each do |index|
+		registration = @registration.reverse					# Reverses the registration for later proccessing
+		(0...registration.size*2).each do |index|
 	  	if index.even?
 	    	@reference << @stock_ref[index/2]
 	  	else
-	    	@reference << @registration[index/2]
+	    	@reference << registration[index/2]
 	 		end
 		end
-		@reference << @stock_ref[10]			# Eventhoug the challenge description says it should
-	end																# take the 9th character, the one taken is the 11th
+		@reference << @stock_ref[8]			# Takes the 9th character as described in the description
+	end																
 
 	def persisted?
     false
--- spec/models/search_engine_spec.rb
+++ spec/models/search_engine_spec.rb
@@ -5,7 +5,7 @@ describe SearchEngine do
   	stock_ref = "ARNFH-U-5728"															# Sample stock reference
   	registration = "SO06DNV"																# Sample registration plate
   	engine = SearchEngine.new(stock_ref, registration)
-  	engine.generate_reference.should eq("AVRNNDF6H0-OUS2") 	# Generates the ofuscated reference for
+  	engine.generate_reference.should eq("AVRNNDF6H0-OUS5") 	# Generates the ofuscated reference for
   end																												# the example
 end
 
@@ -14,18 +14,18 @@ describe SearchEngine do
   	stock_ref = "ARNFH-U-5728"															# Sample stock reference
   	registration = "SO06DNV"																# Sample registration plate
   	engine = SearchEngine.new(stock_ref, registration)
-  	candidate_images = [ "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/350/i/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/350/6/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/350/f/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/350/4/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/350/5/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/350/r/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/800/4/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/800/i/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/800/6/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/800/f/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/800/5/",
-  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS2/800/r/"]	
+  	candidate_images = [ "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/350/4/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/350/i/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/350/6/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/350/f/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/350/5/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/350/r/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/800/4/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/800/i/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/800/6/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/800/f/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/800/5/",
+  											 "http://imagecache.arnoldclark.com/imageserver/AVRNNDF6H0-OUS5/800/r/"]	
   	engine.generate_candidates(engine.generate_reference).should eq(candidate_images) 	# Generates the candidates images for
   end																																										# the example ofuscated reference
 end
