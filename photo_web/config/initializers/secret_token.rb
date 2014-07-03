5d2b4c49b7ac456ffdd76e8e795c147625ad5573
sendorf <sendorf@gmail.com>
2014-07-03 13:44:33 +0200

    Removed duplicated app

--- photo_web/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PhotoWeb::Application.config.secret_token = '84641fe26f91c9075f05cb0d4ca05244610523a4cccae2a9444e7589da7da6ee5e5dfd39e9044d214a0cf315cc1a3b186058c7c452a65f41f408b37056255cc2'
